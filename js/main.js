f5ebd695d2aa54c3c5c79201d6b53f0c103b1cdb
Paweł <110737422+Kosin89@users.noreply.github.com>
2022-10-07 22:39:37 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".nav__hamburger");
+const navMenu = document.querySelector(".nav__menu");
+        
+
+            hamburger.addEventListener("click", () => {
+                hamburger.classList.toggle("active");
+                navMenu.classList.toggle("active");
+            })
+
+document.querySelectorAll(".nav__link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
